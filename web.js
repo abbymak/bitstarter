d6785aada007e9922cd515c93e7767196504324c
Abby Mak <abbymaklf@gmail.com>
2013-07-13 02:00:16 +0000

    Updated web.js

--- web.js
+++ web.js
@@ -4,6 +4,7 @@ var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
   response.send('Hello World3!');
+  response.redirect('~/index.html');
 });
 
 var port = process.env.PORT || 5000;
