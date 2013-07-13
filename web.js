6d11aca256233da9ef3e80173f6d874367767148
Abby Mak <abbymaklf@gmail.com>
2013-07-13 02:25:22 +0000

    Updated web.js

--- web.js
+++ web.js
@@ -2,9 +2,15 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 
+var fs = require('fs'),
+    util = require('util');
+
 app.get('/', function(request, response) {
   response.send('Hello World3!');
-  redirect('~/index.html');
+
+response.writeHead(404, {'content-type': 'text/html'});
+        var rs = fs.createReadStream('index.html');
+        util.pump(rs, response);
 });
 
 var port = process.env.PORT || 5000;
